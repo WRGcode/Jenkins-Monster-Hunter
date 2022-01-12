39da631decd219b4b2f799d92c22920c8dd6bb4d
william reid gill <blueyeboyreid@gmail.com>
2022-01-12 15:31:06 -0700

    start

---
+++ dist/script.js
@@ -0,0 +1,20 @@
+"use strict";
+class Entity {
+    constructor(name, location, challenge, Weaknesses, desription, report) {
+        this.name = name;
+        this.location = location;
+        this.challenge = challenge;
+        this.Weaknesses = Weaknesses;
+        this.desription = desription;
+        this.report = report;
+    }
+}
+//ghosts
+class Ectoplasmic {
+}
+//monsters
+class Beast {
+}
+//humanity(human, elf...) humanlike monsters
+class Humanoids {
+}
---
+++ src/script.ts
@@ -0,0 +1,27 @@
+class Entity {
+  constructor(
+    public name: string, 
+    public location: [number, number],
+    public challenge: number,
+    public Weaknesses: string[],
+
+    public desription: string,
+    public report: string
+
+    ) {}
+}
+
+//ghosts
+class Ectoplasmic{
+
+}
+
+//monsters
+class Beast {
+
+}
+
+//humanity(human, elf...) humanlike monsters
+class Humanoids {
+
+}
